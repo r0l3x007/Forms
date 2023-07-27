679841224cfbdc6ab2d08533f9686ba08a387ae6
Rolex <101451384+r0l3x007@users.noreply.github.com>
2023-07-27 13:49:58 +0300

    Refined password validation added more style to the buttons and text

--- pswvalidation.js
+++ pswvalidation.js
@@ -7,14 +7,13 @@ let pswMsg =  document.getElementById(`pswUnder`);
 function validatepsw(){
 
     if(pass.value.length > 0 && pass.value.length < 8){
-        confirmPsw.style.border = `1px solid rgb(255, 0, 0)`;
+        pass.style.border = `1px solid rgb(255, 0, 0)`;
+        confirmPsw.style.border = `1px solid rgb(255, 0, 0)`
         pswMsg.textContent = `* Password is too short.`
         pswMsg.style.color = `rgb(255, 0, 0)`;
-        return;
-    }
-
-    if(confirmPsw.value == `` || pass.value == ``){
+    } else if(confirmPsw.value == `` || pass.value == ``){
         confirmPsw.style.border = ``;
+        pass.style.border = ``;
         pswMsg.textContent = ``;
     }else if(pass.value != confirmPsw.value && confirmPsw.value != ``){
         confirmPsw.style.border = `1px solid rgb(255, 0, 0)`;
@@ -22,15 +21,13 @@ function validatepsw(){
         pswMsg.style.color = `rgb(255, 0, 0)`;
     } else if (pass.value === confirmPsw.value ){
         confirmPsw.style.border = `1px solid rgb(7, 219, 78)`;
+        pass.style.border = `1px solid rgb(7, 219, 78)`;
         pswMsg.textContent = `* Passwords match`;
         pswMsg.style.color = `rgb(7, 219, 78)`;
     }
 }
 
+
 confirmPsw.addEventListener(`input`, validatepsw);
+pass.addEventListener(`input`, validatepsw);
 
-
-/*else if(pass1.length < 8 && pass2.length < 8 ){
-    confirmPsw.style.border = `1px solid rgb(255, 0, 0)`;
-    pswMsg.textContent = `* Password is too short.`
-    pswMsg.style.color = `rgb(255, 0, 0)`;*/
