f207a4a2dc3c79a01f5f0db7e1c3fa3b08836e70
Rolex <101451384+r0l3x007@users.noreply.github.com>
2023-07-27 15:39:37 +0300

    Added more styling and nuance to the password fields

--- pswvalidation.js
+++ pswvalidation.js
@@ -15,16 +15,16 @@ function validatepsw(){
         confirmPsw.style.border = ``;
         pass.style.border = ``;
         pswMsg.textContent = ``;
-    }else if(pass.value != confirmPsw.value && confirmPsw.value != ``){
+    }else if(pass.value != confirmPsw.value && (pass.value != 0 || confirmPsw.value != ``)){
         confirmPsw.style.border = `1px solid rgb(255, 0, 0)`;
         pswMsg.textContent = `* Passwords do not match.`
         pswMsg.style.color = `rgb(255, 0, 0)`;
-    } else if (pass.value === confirmPsw.value ){
+    } else if (pass.value === confirmPsw.value){
         confirmPsw.style.border = `1px solid rgb(7, 219, 78)`;
         pass.style.border = `1px solid rgb(7, 219, 78)`;
         pswMsg.textContent = `* Passwords match`;
         pswMsg.style.color = `rgb(7, 219, 78)`;
-    }
+    } 
 }
 
 
